be888e3abf8a17f5c97691dd9ac612b788c73ca8
RafaDom10 <r-domingues@outlook.com>
2021-07-18 15:08:08 -0300

    application ready for production

---
+++ src/models/Note.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    favoriteCount: {
+      type: Number,
+      default: 0,
+    },
+
+    favoritedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Note', NoteSchema);
